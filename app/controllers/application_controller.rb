f60a8a5549b6999e59ab64757eef97c45ee48d02
HACHI3232 <harada.chisato526@gmail.com>
2024-01-07 04:23:44 +0900

    deviseがJWTを検証できる仕組みを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  def authenticate_user!
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JWT.decode(header, Rails.application.secrets.secret_key_base)
+      @current_user = User.find(@decoded[0]['user_id'])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+      render json: { errors: ['Unauthorized'] }, status: :unauthorized
+    end
+  end
 end
