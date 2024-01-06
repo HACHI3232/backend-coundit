445b3bf010b8fb97738bd6cb928c03300aa4275b
HACHI3232 <harada.chisato526@gmail.com>
2024-01-07 01:26:44 +0900

    ユーザーコントローラーの設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  # ユーザー登録（サインアップ）のアクション
+  def create
+    user = User.new(user_params)
+    if user.save
+      # 登録成功時の処理
+      render json: { status: 'success', user: user }, status: :created
+    else
+      # 登録失敗時の処理
+      render json: { status: 'error', errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
