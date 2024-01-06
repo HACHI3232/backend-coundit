a7999d36748f1da5e377a1a47ec204a0ffa714b7
HACHI3232 <harada.chisato526@gmail.com>
2024-01-07 04:25:19 +0900

    articleにcreateアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  
+  def create
+    article = current_user.articles.build(article_params)
+    if article.save
+      render json: article, status: :created
+    else
+      render json: article.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description, :body)
+  end
 end
