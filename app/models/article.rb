1dbe93945988df3e3dee6def70864b2c082d2757
HACHI3232 <harada.chisato526@gmail.com>
2024-01-07 16:22:02 +0900

    slugを自動的に生成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
+  before_save :generate_slug
+
+  private
+  def generate_slug
+    self.slug = title.parameterize if title.present? && slug.blank?
+  end
 end
